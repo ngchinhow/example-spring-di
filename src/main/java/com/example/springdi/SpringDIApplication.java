edf81f92a39ca2fb11a59377aa920ddcb1be7120
Ng Chin How <ngchinhow@gmail.com>
2023-06-10 16:06:26 +0800

    Renamed application

---
+++ src/main/java/com/example/springdi/SpringDIApplication.java
@@ -0,0 +1,9 @@
+package com.example.springdi;
+
+public class SpringDIApplication {
+
+	public static void main(String[] args) {
+
+
+	}
+}
--- src/test/java/com/example/springdi/SpringDIApplicationTests.java
+++ src/test/java/com/example/springdi/SpringDIApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.springioc;
+package com.example.springdi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringIocApplicationTests {
+class SpringDIApplicationTests {
 
 	@Test
 	void contextLoads() {
